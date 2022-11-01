9d72f94c5e66867646f172105389626cc0014e26
Sadie Allen <sadiela@bu.edu>
2022-11-01 16:23:43 -0400

    made a python file

---
+++ fenchel_loop.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+
+def OMD():
+    return 0
+
+def FTL():
+    return 0
+
+if __name__ == "__main__":
+    # schedules
+    T = 10000 # number of rounds
+    alpha_ts = 1 # or a  vector of length T 
+    # Pick to algorithms
+    for i in range(T): 
+        
+
+
+    # Pair up "fair players" FTL & OMD
+
+    # what functions are we interested in optimizing? How hard is it to calc the fenchel conjugate?!?
+        # power function
+        # search OCO literature
