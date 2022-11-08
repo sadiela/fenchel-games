61773d5543ce114d26c2f1f04320e130744e1569
Sadie Allen <sadiela@bu.edu>
2022-11-08 15:20:21 -0500

    pull

--- fenchel_loop.py
+++ fenchel_loop.py
@@ -1,7 +1,16 @@
 import numpy as np 
 
 
+def f():
+    return 0
+
+def g(x,y):
+    return 0 
+
 def OMD():
+    return 0
+
+def OOMD():
     return 0
 
 def FTL():
@@ -9,15 +18,21 @@ def FTL():
 
 if __name__ == "__main__":
     # schedules
+    x_t = np.random.rand(10)
+    y_t = np.random.rand(10)
     T = 10000 # number of rounds
     alpha_ts = 1 # or a  vector of length T 
     # Pick to algorithms
     for i in range(T): 
-        
+        print(i)
 
 
     # Pair up "fair players" FTL & OMD
 
     # what functions are we interested in optimizing? How hard is it to calc the fenchel conjugate?!?
-        # power function
-        # search OCO literature
+    # power function
+    # search OCO literature
+    # convex loss functions? square loss, hinge loss, smoothed hinge loss, modified square loss, exponential loss function
+    # log loss function 
+    # which loss function/OL algorithm combos are "allowed"
+    # https://core.ac.uk/download/pdf/213011306.pdf
