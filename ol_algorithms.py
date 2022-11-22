adcb91bb65ec14b9a39fb28df73f5173c99292df
nicksacco17 <nicksacco17@gmail.com>
2022-11-21 23:15:25 -0600

    Prelimary Optimistic FTRL using previous loss

--- ol_algorithms.py
+++ ol_algorithms.py
@@ -50,9 +50,9 @@ class FTL:
         self.name = "FTL"
         self.f = f
         self.d = d
-        self.z0 = z0
         self.alpha_t = weights
-
+        self.z0 = z0
+        
     def get_update_y(self, x, t):
 
         if t == 0:
@@ -66,9 +66,56 @@ class FTL:
 
 class FTRL:
 
-    def __init__(self):
+    def __init__(self, f, d, weights, eta, z0):
         self.name = "FTRL"
+        self.f = f
+        self.d = d
+        self.alpha_t = weights
+        self.z0 = z0
+        self.eta = eta  
         self.regularizer = r"$\frac{1}{2} \sqrt{t} ||x||^2$"
 
-    def get_update(self, g, t):
-        return -np.sum(g, axis = 1) / np.sqrt(t) # R(x,t) = 1/2 sqrt(t) ||x||^2
+    # Assumes R(x,t) = 1/2 sqrt(t) ||x||^2
+    def get_update_x(self, g, t):
+        return -np.average(g, axis = 1, weights = self.alpha_t[0:t]) * np.sum(self.alpha_t[0:t]) / np.sqrt(t)
+
+
+class OFTRL:
+
+    def __init__(self, f, d, weights, hints, z0):
+        self.name = "Optimistic FTRL"
+        self.f = f
+        self.d = d
+        self.alpha_t = weights
+        self.hints = None       # Uses hint m(t) = \ell(t-1), the previous loss
+        self.z0 = z0
+
+    # This is the closed form for the update of x - the x update is really easy,
+    # since there is no dependence on the Fenchel conjugate.
+
+    # DOUBLE CHECK THERE ARE NO OFF-BY-ONE ERRORS!
+    def get_update_x(self, y, t):
+
+        weighted_sum = np.zeros(shape = (self.d))
+        for i in range(0, t-1):
+            weighted_sum += self.alpha_t[i] * y[i]
+        weighted_sum += self.alpha_t[t] * y[t-1]
+
+        return -weighted_sum / np.sqrt(t)
+
+    # This update rule is tightly coupled to the Fenchel conjugate... not sure
+    # how to decouple it to get "general" update rules that are independent of 
+    # the specific function we are trying to implement...maybe a numerical
+    # solution?  We can get decent closed form expressions, maybe we can compute
+    # an equality...
+
+    # DOUBLE CHECK THERE ARE NO OFF-BY-ONE-ERRORS!
+    def get_update_y(self, x, t):
+        
+        weighted_sum = np.zeros(shape = (self.d))
+        for i in range(0, t-1):
+            weighted_sum += self.alpha_t[i] * x[i]
+        weighted_sum + self.alpha_t[t] * x[t-1]
+
+        return weighted_sum / (np.sqrt(t) + np.sum(self.alpha_t[0:t]))
+
