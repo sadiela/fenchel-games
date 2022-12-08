Refined test suite, added more weight schedules, fixed some off by one errors

algorithm_comparison.py
@@ -5,6 +5,36 @@ from fenchel_loop import *
 from convex_optimization_algorithms import *
 from ol_algorithms import *
 
+class Weights:
+
+    def __init__(self, name, T):
+        self.name = name
+        self.T = T
+
+        if self.name == "ones":
+            self.weights = np.ones(self.T + 1)
+        elif self.name == "linear":
+            self.weights = np.linspace(0, self.T, self.T + 1)
+        elif self.name == "sqrt":
+            self.weights = [np.sqrt(t) for t in range(0, self.T + 1)]
+            self.weights[0] = 0
+        elif self.name == "log":
+            self.weights = [np.log(t) for t in range(0, self.T + 1)]
+            self.weights[0] = 0
+
+    def print_weights(self):
+
+        for t in range(0, self.T):
+            print("\u03B1[%d] = %lf" % (t, self.weights[t]))
+
+    def plot_weights(self):
+
+        plt.plot(self.weights, '-b', linewidth = 1.5)
+        plt.title("Weight schedule: " + self.name)
+        plt.show()
+
+
+
 def run_helper(f_game, x_alg, y_alg, T, d, weights, xbounds, ybounds, yfirst = True):
 
     print("Running algorithms X = " + x_alg.name + ", Y = " + y_alg.name)
@@ -17,15 +47,15 @@ def run_helper(f_game, x_alg, y_alg, T, d, weights, xbounds, ybounds, yfirst = T
     print("Saddle Point (x*, y*) = (%0.3f, %0.3f)" % (m_game.x_star, m_game.y_star))
     print("Final iterate:", m_game.x[-1], m_game.y[-1])
 
-    game_xbar = m_game.xbar
+    #game_xbar = m_game.xbar
 
-    print(game_xbar[-1]/np.linalg.norm(game_xbar[-1]))
+    #print(game_xbar[-1]/np.linalg.norm(game_xbar[-1]))
 
-    plt.figure()
+    #plt.figure()
     #plt.plot(x_ts_n_onemem[1:], color = 'blue', label = "NesterovsOne-Memory")
     #plt.plot(x_ts_n_infmem[1:], color = 'blue', label = "NesterovsInf-Memory")
-    plt.plot([x[0]/np.linalg.norm(x) for x in game_xbar], [x[1]/np.linalg.norm(x) for x in game_xbar], color = 'red', linestyle = '--', label = 'FGNRD Recovery')
-    plt.show()
+    #plt.plot([x[0]/np.linalg.norm(x) for x in game_xbar], [x[1]/np.linalg.norm(x) for x in game_xbar], color = 'red', linestyle = '--', label = 'FGNRD Recovery')
+    #plt.show()
 
 
     #m_game.plot_xbar()
@@ -42,44 +72,296 @@ def run_helper(f_game, x_alg, y_alg, T, d, weights, xbounds, ybounds, yfirst = T
 
     #print(m_game.acl_x)
 
-    #return m_game.xbar, m_game.x
+    return m_game.xbar, m_game.x
 
 def FW_Recovery():
-
-    # ---------------------------- RUN FENCHEL GAME ----------------------------
-
-    ftrl = FTRL(f = f_game, d=d, weights=alpha_t, eta=0, z0=0)
-    
-    # --------------------------------------------------------------------------
-
-    # ---------------------- RUN FRANK-WOLFE OPTIMIZATION ----------------------
-
-    xbounds = [-1,1]
-    
-    #x_t = np.array([10,-10])
-    #print(x_t)
-
-    #x_ts = frankWolfe(f_opt, T, X_INIT, L=1) #(f_opt, T, X_INIT, xbounds)
-    x_ts = frankWolfe(f_opt, T, X_INIT, xbounds)
-
-    plt.figure()
-    plt.title("Frank-Wolfe <--> X: BestResp+, Y: FTL")
-    plt.plot(x_ts[1:], '-r', label = "FW")
-    plt.plot(m_game.xbar, '--b', label = "BR + FTL")
-    plt.legend()
-    plt.show()
-
-
-if __name__ == '__main__':
-
-    #print("Salve Munde")
-    #FW_Recovery()
 
     T = 100
     d = 1
 
+    alpha_t = Weights("linear", T = T)
+   
+    XBOUNDS = [[-10, 10]]
+    YBOUNDS = [[-10, 10]]
+
+    f_game = PowerFenchel(p = 2, q = 2)
+    f_opt = PowerFunction(p = 2, q = 2)
+
+    x_0 = np.array([5], dtype='float64')
+
+    x_ts_FW = frankWolfe(f = f_opt, T = T, w_0 = x_0, xbounds = XBOUNDS)
+
+    bestresp = BestResponse(f = f_game, d = d, weights = alpha_t.weights, z0 = x_0, xbounds = XBOUNDS, ybounds = YBOUNDS)
+    ftl = FTL(f = f_game, d = d, weights = alpha_t.weights, z0 = f_opt.grad(x_0), bounds = YBOUNDS)
+  
+    game_xbar, _ = run_helper(f_game = f_game, x_alg = bestresp, y_alg = ftl, T = T + 1, d = d, weights = alpha_t.weights, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = True)
+           
+    plt.figure()
+    plt.title("Algorithm Recovery: Frank-Wolfe <--> X: BestResp+, Y: FTL")
+    plt.plot(x_ts_FW[1:], color = 'blue', linewidth = 1.5, label = "Frank-Wolfe")
+    plt.plot(game_xbar[1:], color = 'red', linewidth = 1.5, linestyle = '--', label = "FGNRD Recovery")
+    plt.legend()
+    plt.show()
+
+def GDwAVG_Recovery():
+
+    T = 100
+    d = 1
+
+    alpha_t = Weights("ones", T = T)
+    eta_t = 0.5 * np.ones(T+1)
+   
+    XBOUNDS = [[-10, 10]]
+    YBOUNDS = [[-10, 10]]
+
+    f_game = PowerFenchel(p = 2, q = 2) #ExpFenchel()
+    f_opt = PowerFunction(p = 2, q = 2)
+
+    x_0 = np.array([5], dtype='float64')
+
+    
+    x_ts_GDAvg = gradDescentAveraging(f = f_opt, T = T, w_0 = x_0, L = 1) 
+
+    bestresp = BestResponse(f = f_game, d = d, weights = alpha_t.weights, z0 = x_0, xbounds = XBOUNDS, ybounds = YBOUNDS)
+    omd = OMD(f = f_game, d = d, weights = alpha_t.weights, z0 = x_0, y0 = x_0, eta_t = eta_t, bounds = XBOUNDS)
+  
+    game_xbar, _ = run_helper(f_game = f_game, x_alg = omd, y_alg = bestresp, T = T + 1, d = d, weights = alpha_t.weights, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = False)
+           
+    plt.figure()
+    plt.title("Algorithm Recovery: Gradient Descent w/Averaging <--> X: OMD, Y: FTL+")
+    plt.plot(x_ts_GDAvg, color = 'blue', linewidth = 1.5, label = "GDw/AVG")
+    plt.plot(game_xbar[1:T], color = 'red', linewidth = 1.5, linestyle = '--', label = "FGNRD Recovery")
+    plt.legend()
+    plt.show()
+
+def CGD_Recovery():
+
+    T = 10
+    d = 1
+
+    R = 10
+    G = 1
+
+    alpha_t = Weights("ones", T = T)
+    eta_t = (R / (G * np.sqrt(T))) * np.ones(T+1)
+   
+    XBOUNDS = [[-10, 10]]
+    YBOUNDS = [[-10, 10]]
+
+    f_game = PowerFenchel(p = 2, q = 2) #ExpFenchel()
+    f_opt = PowerFunction(p = 2, q = 2)
+
+    x_0 = np.array([5], dtype='float64')
+
+    x_ts_cumulativeGD = cumulativeGradientDescent(f = f_opt, T = T, w_0 = x_0, R = R, G = G)
+
+    omd = OMD(f = f_game, d = d, weights = alpha_t.weights, z0 = x_0, y0 = x_0, eta_t = eta_t, bounds = XBOUNDS)
+    ftl = FTL(f = f_game, d = d, weights = alpha_t.weights, z0 = f_opt.grad(x_0), bounds = YBOUNDS, prescient = True)
+
+    game_xbar, _ = run_helper(f_game = f_game, x_alg = omd, y_alg = ftl, T = T + 1, d = d, weights = alpha_t.weights, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = False)
+           
+    print(x_ts_cumulativeGD)
+    print(game_xbar)
+
+        
+    plt.figure()
+    plt.title("Algorithm Recovery: Cumulative Gradient Descent <--> X: OMD, Y: FTL+")
+    plt.plot(x_ts_cumulativeGD[1:], color = 'blue', linewidth = 1.5, label = "CGD")
+    plt.plot(game_xbar[1:], color = 'red', linewidth = 1.5, linestyle = '--', label = "FGNRD Recovery")
+    plt.legend()
+    plt.show()
+
+def SCEGwAVG_Recovery():
+
+    T = 10
+    d = 1
+
+    L = 1
+
+    alpha_t = Weights("ones", T = T)
+    eta_t = 0.5 * np.ones(T+1)
+   
+    XBOUNDS = [[-10, 10]]
+    YBOUNDS = [[-10, 10]]
+
+    f_game = PowerFenchel(p = 2, q = 2) #ExpFenchel()
+    f_opt = PowerFunction(p = 2, q = 2)
+
+    x_0 = np.array([5], dtype='float64')
+    phi = L2Reg()
+
+    x_ts_sgc_eg = singleGradientCallExtraGradientWithAveraging(f = f_opt, T = T, w_0 = x_0, phi = phi)
+
+    optimistic_omd = OOMD(f = f_game, d = d, weights = alpha_t.weights, x0 = x_0, xminushalf = x_0, y0 = x_0, yminushalf = x_0, eta_t = eta_t, bounds = XBOUNDS, yfirst = False)
+    bestresp = BestResponse(f = f_game, d = d, weights = alpha_t.weights, z0 = x_0, xbounds = XBOUNDS, ybounds = YBOUNDS)
+
+    game_xbar, _ = run_helper(f_game = f_game, x_alg = optimistic_omd, y_alg = bestresp, T = T + 1, d = d, weights = alpha_t.weights, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = False)
+           
+    plt.figure()
+    plt.title("Algorithm Recovery: Single-Call Extra-Gradient with Averaging <--> X: Opt OMD, Y: BR+")
+    plt.plot(x_ts_sgc_eg[1:], color = 'blue', linewidth = 1.5, label = "SCEGw/AVG")
+    plt.plot(game_xbar[1:], color = 'red', linewidth = 1.5, linestyle = '--', label = "FGNRD Recovery")
+    plt.legend()
+    plt.show()
+
+def Nesterov1Mem_Recovery():
+
+    T = 100
+    d = 1
+
+    L = 1
+
+    alpha_t = Weights("linear", T = T)
+    alpha_t.print_weights()
+    eta_t = 0.25 * np.ones(T+1)
+   
+    XBOUNDS = [[-10, 10]]
+    YBOUNDS = [[-10, 10]]
+
+    f_game = PowerFenchel(p = 2, q = 2) #ExpFenchel()
+    f_opt = PowerFunction(p = 2, q = 2)
+
+    x_0 = np.array([5], dtype='float64')
+    phi = L2Reg()
+
+    x_ts_n_onemem, _ = nesterovOneMemory(f = f_opt, T = T, w_0 = x_0, phi = phi, L = 1)  
+
+    omd = OMD(f = f_game, d = d, weights = alpha_t.weights, z0 = x_0, y0 = x_0, eta_t = eta_t, bounds = XBOUNDS, prescient = True)
+    optimistic_ftl = OFTL(f = f_game, d = d, weights = alpha_t.weights, z0 = f_opt.grad(x_0), bounds = YBOUNDS)
+
+    game_xbar, _ = run_helper(f_game = f_game, x_alg = omd, y_alg = optimistic_ftl, T = T + 1, d = d, weights = alpha_t.weights, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = True)
+           
+    plt.figure()
+    plt.title("Algorithm Recovery: Nesterov's 1-Memory Method <--> X: OMD+, Y: Opt-FTL")
+    plt.plot(x_ts_n_onemem[1:], color = 'blue', linewidth = 1.5, label = "Nesterov's 1Mem")
+    plt.plot(game_xbar[1:], color = 'red', linewidth = 1.5, linestyle = '--', label = "FGNRD Recovery")
+    plt.legend()
+    plt.show()
+
+def NesterovInfMem_Recovery():
+
+    T = 100
+    d = 1
+
+    L = 1
+
+    alpha_t = Weights("linear", T = T)
+    alpha_t.print_weights()
+    eta_t = 0.25 * np.ones(T+1)
+    eta = 0.25
+   
+    XBOUNDS = [[-10, 10]]
+    YBOUNDS = [[-10, 10]]
+
+    f_game = PowerFenchel(p = 2, q = 2) #ExpFenchel()
+    f_opt = PowerFunction(p = 2, q = 2)
+
+    x_0 = np.array([5], dtype='float64')
+    phi = L2Reg()
+
+    x_ts_n_infmem, _ = nesterovInfMemory(f = f_opt, T = T, w_0 = x_0, R = phi, L = 1) 
+
+    ftrl = FTRL(f = f_game, d = d, weights = alpha_t.weights, z0 = np.array([5.0]), eta = eta, reg = phi, bounds = XBOUNDS, prescient = True)
+    optimistic_ftl = OFTL(f = f_game, d = d, weights = alpha_t.weights, z0 = f_opt.grad(x_0), bounds = YBOUNDS)
+
+    game_xbar, _ = run_helper(f_game = f_game, x_alg = ftrl, y_alg = optimistic_ftl, T = T + 1, d = d, weights = alpha_t.weights, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = True)
+           
+    plt.figure()
+    plt.title("Algorithm Recovery: Nesterov's Inf-Memory Method <--> X: FTRL+, Y: Opt-FTL")
+    plt.plot(x_ts_n_infmem[1:], color = 'blue', linewidth = 1.5, label = "Nesterov's 1Mem")
+    plt.plot(game_xbar[1:], color = 'red', linewidth = 1.5, linestyle = '--', label = "FGNRD Recovery")
+    plt.legend()
+    plt.show()
+
+def HeavyBall_Recovery():
+
+    T = 10
+    d = 1
+    L = 1
+
+    alpha_t = Weights("linear", T = T)
+    alpha_t.print_weights()
+    eta_t = 0.125 * np.ones(T+1)
+   
+    XBOUNDS = [[-10, 10]]
+    YBOUNDS = [[-10, 10]]
+
+    f_game = PowerFenchel(p = 2, q = 2) #ExpFenchel()
+    f_opt = PowerFunction(p = 2, q = 2)
+
+    x_0 = np.array([5], dtype='float64')
+    phi = L2Reg()
+
+    x_ts_heavyball = heavyBall(f = f_opt, T = T, w_0 = x_0, L = 1) 
+
+    omd = OMD(f = f_game, d = d, weights = alpha_t.weights, z0 = x_0, y0 = x_0, eta_t = eta_t, bounds = XBOUNDS, prescient = True)
+    ftl = FTL(f = f_game, d = d, weights = alpha_t.weights, z0 = f_opt.grad(x_0), bounds = YBOUNDS, prescient = False)
+  
+    game_xbar, _ = run_helper(f_game = f_game, x_alg = omd, y_alg = ftl, T = T + 1, d = d, weights = alpha_t.weights, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = True)
+           
+    print(x_ts_heavyball)
+    print(game_xbar)
+        
+    plt.figure()
+    plt.title("Algorithm Recovery: Heavy Ball <--> X: OMD+, Y: FTL")
+    plt.plot(x_ts_heavyball[2:], color = 'blue', linewidth = 1.5, label = "Heavy Ball")
+    plt.plot(game_xbar[1:], color = 'red', linewidth = 1.5, linestyle = '--', label = "FGNRD Recovery")
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+
+    #print("Salve Munde")
+    
+    # STATUS: OPERATIONAL
+    # alpha_t = t
+    #FW_Recovery()
+
+    # STATUS: OPERATIONAL
+    # alpha_t = 1, eta_t = (1/2)*L
+    #GDwAVG_Recovery()
+
+    # STATUS: CLOSE BUT NOT EXACT - NEED TO FIX PRESCIENT FLAG
+    # alpha_t = 1, eta_t = R/Gsqrt(T)
+    #CGD_Recovery()
+
+    # STATUS: OPERATIONAL
+    # alpha_t = 1, eta_t = (1/2)*L
+    #SCEGwAVG_Recovery()
+
+    # STATUS: OPERATIONAL
+    # alpha_t = t, eta_t = (1/2)*L
+    #Nesterov1Mem_Recovery()
+
+    # STATUS: OPERATIONAL
+    # alpha_t = t, eta_t = (1/4)*L
+    #NesterovInfMem_Recovery()
+
+    # STATUS: OPERATIONAL
+    # alpha_t = t, eta_t = (1/8)*L
+    #HeavyBall_Recovery()
+
+    T = 100
+
+    weight_schedules = ["ones", "linear", "sqrt", "log"]
+
+    for wt in weight_schedules:
+
+        alpha_t = Weights(name = wt, T = T)
+        alpha_t.plot_weights()
+
+
+'''
+    T = 100
+    d = 1
+
+    alpha_t = Weights("linear", T = T)
+    alpha_t.print_weights()
+    alpha_t.plot_weights()
+
+
     #alpha_t = np.linspace(1, T, T)
-    alpha_t = np.ones(T)
+    #alpha_t = np.ones(T)
     eta_t = 0.5*np.ones(T)
 
     XBOUNDS = [[-10, 10]]
@@ -95,7 +377,9 @@ if __name__ == '__main__':
 
     x_0 = np.array([5], dtype='float64')
 
-    # alpha_t = t, eta_t = (1/4)*L
+
+    x_ts_FW = frankWolfe(f = f_opt, T = T, w_0 = x_0, xbounds = XBOUNDS)
+    # alpha_t = t, eta_t = (1/2)*L
     #x_ts_n_onemem, v_ts = nesterovOneMemory(f = f_opt, T = T, w_0 = x_0, phi = phi, L = 1)  
 
     # alpha_t = t, eta_t = (1/4)*L
@@ -112,27 +396,32 @@ if __name__ == '__main__':
     #f_opt = SqrtOneXSquared()
 
     # OL ALGORITHMS
-    bestresp = BestResponse(f = f_game, d = d, weights = alpha_t, xbounds = XBOUNDS, ybounds = YBOUNDS)
-    ftl = FTL(f = f_game, d = d, weights = alpha_t, z0 = np.array([5.0]), bounds = YBOUNDS)
-    optimistic_ftl = OFTL(f = f_game, d = d, weights = alpha_t, z0 = f_opt.grad(np.array([5.0])), bounds = YBOUNDS)
+    bestresp = BestResponse(f = f_game, d = d, weights = alpha_t.weights, z0 = x_0, xbounds = XBOUNDS, ybounds = YBOUNDS)
+    ftl = FTL(f = f_game, d = d, weights = alpha_t.weights, z0 = f_opt.grad(x_0), bounds = YBOUNDS)
+    #optimistic_ftl = OFTL(f = f_game, d = d, weights = alpha_t, z0 = f_opt.grad(np.array([5.0])), bounds = YBOUNDS)
 
-    ftrl = FTRL(f = f_game, d = d, weights = alpha_t, z0 = np.array([5.0]), eta = 0.25, reg = phi, bounds = XBOUNDS)
+    #ftrl = FTRL(f = f_game, d = d, weights = alpha_t, z0 = np.array([5.0]), eta = 0.25, reg = phi, bounds = XBOUNDS)
 
-    optimistic_ftrl = OFTRL(f = f_game, d = d, weights = alpha_t, z0 = np.array([1.0]), bounds = XBOUNDS)
-    omd = OMD(f = f_game, d = d, weights = alpha_t, z0 = np.array([5.0]), y0 = np.array([5.0]), eta_t = eta_t, bounds = XBOUNDS)
+    #optimistic_ftrl = OFTRL(f = f_game, d = d, weights = alpha_t, z0 = np.array([1.0]), bounds = XBOUNDS)
+    #omd = OMD(f = f_game, d = d, weights = alpha_t, z0 = np.array([5.0]), y0 = np.array([5.0]), eta_t = eta_t, bounds = XBOUNDS)
 
-    optimistic_omd = OOMD(f = f_game, d = d, weights = alpha_t, x0 = np.array([5.0]), xminushalf = np.array([5.0]), y0 = np.array([5.0]), yminushalf = np.array([0]), eta_t = eta_t, bounds = XBOUNDS, yfirst = False)
+    #optimistic_omd = OOMD(f = f_game, d = d, weights = alpha_t, x0 = np.array([5.0]), xminushalf = np.array([5.0]), y0 = np.array([5.0]), yminushalf = np.array([0]), eta_t = eta_t, bounds = XBOUNDS, yfirst = False)
 
-    X_LIST = [ftrl, optimistic_ftrl, omd, optimistic_omd]
-    Y_LIST = [ftl, optimistic_ftl]
+    #X_LIST = [ftrl, optimistic_ftrl, omd, optimistic_omd]
+    #Y_LIST = [ftl, optimistic_ftl]
 
     #run_helper(f_game = f_game, x_alg = optimistic_omd, y_alg = bestresp, T = T, d = d, weights = alpha_t, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = False)
         
-    for x in X_LIST:
-        for y in Y_LIST:
-            print("RUNNING ALGORITHM PAIR (" + x.name + ", " + y.name + ")")
-            run_helper(f_game = f_game, x_alg = x, y_alg = y, T = T, d = d, weights = alpha_t, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = True)
-            print()
+    #for x in X_LIST:
+    #    for y in Y_LIST:
+    #        print("RUNNING ALGORITHM PAIR (" + x.name + ", " + y.name + ")")
+    #        game_xbar, xt = run_helper(f_game = f_game, x_alg = x, y_alg = y, T = T, d = d, weights = alpha_t, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = True)
+    #        print()
+
+
+    game_xbar, xt = run_helper(f_game = f_game, x_alg = bestresp, y_alg = ftl, T = T + 1, d = d, weights = alpha_t.weights, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = True)
+           
+
 
     #game_xbar, xt = run_helper(f_game = f_game, f_opt = f_opt, x_alg = optimistic_omd, y_alg = bestresp, T = T, d = d, weights = alpha_t, xbounds = XBOUNDS, ybounds = YBOUNDS, yfirst = False)
 
@@ -143,21 +432,22 @@ if __name__ == '__main__':
     #plt.figure()
 
     #t_plot = np.linspace(0, T, T+1)
+    plt.plot(x_ts_FW[1:], color = 'blue', label = "Frank-Wolfe")
     #plt.plot(x_ts_n_onemem[1:], color = 'blue', label = "NesterovsOne-Memory")
     #plt.plot(x_ts_n_infmem[1:], color = 'blue', label = "NesterovsInf-Memory")
     #plt.plot(t_plot[0:len(x_ts_GDAvg)], x_ts_GDAvg[0:], color = 'blue', label = "Gradient Descent w/ Averaging")
     #plt.plot(x_ts_sgc_eg[1:], color = 'blue', label = "Single-Call Extra Gradient with Averaging")
-    #plt.plot(game_xbar, color = 'red', linestyle = '--', label = 'FGNRD Recovery')
+    plt.plot(game_xbar[1:], color = 'red', linestyle = '--', label = 'FGNRD Recovery')
     #plt.plot(v_ts[1:], color = 'green', linestyle = '--', label = 'vts')
     #plt.plot(xt, color = 'purple', linestyle = '--', label = 'xt')
     #plt.plot(x_ts_n_infmem, color='gray', label="NesterovsInf-Memory")
 
-    #plt.title("Algorithm Recovery")
-    #plt.legend()
-    #plt.show()
+    plt.title("Algorithm Recovery")
+    plt.legend()
+    plt.show()
 
     #print(x_ts_n_onemem)
     #print(game_xbar)
-
+'''
 
     

fenchel_loop.py
@@ -40,8 +40,8 @@ class Fenchel_Game:
         #self.loss_x = [(self.alpha[0] * self.f.payoff(self.x[0], self.y[0]))]
         #self.loss_y = [(self.alpha[0] * -self.f.payoff(self.x[0], self.y[0]))]
 
-        self.loss_x = []
-        self.loss_y = []
+        self.loss_x = [0]
+        self.loss_y = [0]
 
         # Update gradients
         #self.gx = [self.f.grad_x(self.x[0], self.y[0])] #np.zeros(shape = (self.d, self.T+1), dtype = float)
@@ -59,6 +59,9 @@ class Fenchel_Game:
     def set_players(self, x_alg, y_alg):
         self.algo_X = x_alg
         self.algo_Y = y_alg
+
+        self.x.append(self.algo_X.z0)
+        self.y.append(self.algo_Y.z0)
 
     def set_teams(self, x_team, y_team, lr = 0.5, wtx1 = (0.5, 0.75), wty1 = (0.5, 0.75)):
         self.team_X = x_team
@@ -89,7 +92,7 @@ class Fenchel_Game:
     def run(self, yfirst = True):
         #print(self.y[-1], self.x[-1])
 
-        for t in range(0, self.T):
+        for t in range(1, self.T):
 
             #if t % (self.T/10) == 0:
             #    print("Updating round t = %d" % t)
@@ -100,7 +103,7 @@ class Fenchel_Game:
                 #self.y[:, t] = np.minimum(np.maximum(self.algo_Y.get_update(self.gy[:, 0:t], t+1), self.ybounds[0][0]), self.ybounds[0][1])
                 
                 self.y.append(projection(self.algo_Y.get_update_y(self.x, t), self.ybounds))
-                #debug_print("y[%d] = %lf" % (t, self.y[-1]), self.T)
+                debug_print("y[%d] = %lf" % (t, self.y[-1]), self.T)
                 
                 # Compute the subgradients
                 #debug_print("--> Update gx[%d], using f(x, y[%d])" % (t, t), self.T)
@@ -111,10 +114,10 @@ class Fenchel_Game:
                 #debug_print("--> Return x[%d], computing with N = %d gradients from t = 0 to t = %d" % (t, len(self.gx), t), self.T)
                 #self.x[:, t] = np.minimum(np.maximum(self.algo_X.get_update(self.gx[:, 0:t+1], t+1), self.xbounds[0][0]), self.xbounds[0][1]) # WILL HAVE TO CHANGE FOR LARGER DIMS
                 self.x.append(projection(self.algo_X.get_update_x(self.y, t), self.xbounds))
-                #self.loss_x.append((self.alpha[t] * self.f.payoff(self.x[-1], self.y[-1])))
+                
                 #self.acl_x.append(np.sum(self.loss_x) / np.sum(self.alpha[0:t+1]))
 
-                #debug_print("x[%d] = %lf" % (t, self.x[-1]), self.T)
+                debug_print("x[%d] = %lf" % (t, self.x[-1]), self.T)
                 #debug_print("lx[%d] = \u03B1[%d] * g(x[%d], y[%d]) = %lf" % (t, t, t, t, self.loss_x[-1]), self.T)
 
                 # new gy subgradient
@@ -122,6 +125,7 @@ class Fenchel_Game:
                 #self.gy.append(-self.alpha[t] * self.f.grad_y(self.x[-1], self.y[-1])) # 1 - self.x[:, t]
                 #debug_print("gy[%d] = %lf" % (t+1, self.gy[-1]), self.T)
 
+                #self.loss_x.append((self.alpha[t] * self.f.payoff(self.x[-1], self.y[-1])))
                 #self.loss_y.append((self.alpha[t] * -self.f.payoff(self.x[-1], self.y[-1])))
                 #self.acl_y.append(np.sum(self.loss_y) / np.sum(self.alpha[0:t+1]))
                 #debug_print("ly[%d] = -\u03B1[%d] * g(x[%d], y[%d]) = %lf" % (t, t, t-1, t, self.loss_y[-1]), self.T)
@@ -132,6 +136,9 @@ class Fenchel_Game:
                 self.y.append(projection(self.algo_Y.get_update_y(self.x, t), self.ybounds))
                 debug_print("y[%d] = %lf" % (t, self.y[-1]), self.T)
 
+            self.loss_x.append((self.alpha[t] * self.f.payoff(self.x[-1], self.y[-1])))
+            self.loss_y.append((self.alpha[t] * -self.f.payoff(self.x[-1], self.y[-1])))
+
             if self.algo_X.name == "Opt-OMD":
                 self.algo_X.update_half(self.y, t)
         
@@ -141,12 +148,14 @@ class Fenchel_Game:
 
         print("Fenchel game complete, T = [%d, %d, %d] rounds" % (self.T, len(self.x), len(self.y)))
 
-        weighted_sum = self.x[0] * self.alpha[0]
-        self.xbar = [weighted_sum / self.alpha[0]] 
+        #weighted_sum = self.x[0] * self.alpha[0]
+        #self.xbar = [weighted_sum / self.alpha[0]] 
 
+        weighted_sum = np.zeros((self.d))
+        self.xbar = [weighted_sum]
         for t in range(1, self.T):
             weighted_sum += (self.alpha[t] * self.x[t])
-            self.xbar.append(weighted_sum / np.sum(self.alpha[0:t+1]))
+            self.xbar.append(weighted_sum / np.sum(self.alpha[1:t+1]))
         #self.x_star = np.average(np.concatenate(self.x, axis=0), weights = self.alpha, axis=0)
         #self.y_star = np.average(np.concatenate(self.y, axis=0), weights = self.alpha, axis=0)
 

ol_algorithms.py
@@ -7,11 +7,12 @@ import numpy as np
 class BestResponse: # implemented for power function only 
 
     # Might need some projections here...This is also very messy, need to be careful about who is actually running BestResp
-    def __init__(self, f, d, weights, xbounds, ybounds):
+    def __init__(self, f, d, z0, weights, xbounds, ybounds):
         self.name = "BestResp"
         self.f = f   
         self.d = d   
         self.alpha_t = weights
+        self.z0 = z0
         self.xbounds = xbounds
         self.ybounds = ybounds
 
@@ -30,21 +31,32 @@ class BestResponse: # implemented for power function only
 
 class OMD():
 
-    def __init__(self, f, d, weights, z0, y0, eta_t, bounds):
+    def __init__(self, f, d, weights, z0, y0, eta_t, bounds, prescient = False):
         self.name = "OMD"
         self.f = f
         self.d = d
         self.alpha_t = weights
         self.eta_t = eta_t
+        self.z0 = z0
         self.z = z0
         self.y0 = y0
         self.bounds = bounds
+        self.prescient = prescient
 
     def get_update_x(self, y, t):
-        if len(y) > 0:
-            self.z = self.z - self.eta_t[t] * self.alpha_t[t] * y[-1]
-        else:
-            self.z = self.z - self.eta_t[t] * self.alpha_t[t] * self.y0
+
+        if t == 1 and not self.prescient:
+            self.z = self.z = self.eta_t[t] * self.alpha_t[t] * self.y0
+        elif self.prescient:
+            #print("Start t = %d, z = %lf:" % (t, self.z))
+            self.z = self.z - (self.eta_t[t] * self.alpha_t[t] * y[-1])
+            #print("%f %lf %f" % (self.eta_t[t], self.alpha_t[t], y[-1]))
+            #print(self.eta_t[t] * self.alpha_t[t] * y[-1])
+
+        #if len(y) > 0:
+        #    self.z = self.z - self.eta_t[t] * self.alpha_t[t] * y[-1]
+        #else:
+        #    self.z = self.z - self.eta_t[t] * self.alpha_t[t] * self.y0
         #print("Before projection: ", self.z)
         return self.z
         
@@ -59,6 +71,7 @@ class OOMD():
         self.d = d
         self.alpha_t = weights
         self.eta_t = eta_t
+        self.z0 = x0
         self.z = x0
         self.z_half = xminushalf
         self.y0 = y0
@@ -112,7 +125,7 @@ class OOMD():
 
 class FTL:
 
-    def __init__(self, f, d, weights, z0, bounds):
+    def __init__(self, f, d, weights, z0, bounds, prescient = False):
         self.name = "FTL"
         self.f = f
         self.d = d
@@ -120,17 +133,21 @@ class FTL:
         self.z0 = z0
         self.weighted_sum = np.zeros(shape = (self.d))
         self.bounds = bounds
+        self.prescient = prescient
 
     def get_update_y(self, x, t):
 
-        if t == 0:
+        if t == 1 and not self.prescient:
             #self.weighted_sum += self.alpha_t[t] * self.z0
             return self.z0
-        else:
+        elif not self.prescient:
             self.weighted_sum += self.alpha_t[t-1] * x[-1]
-            update = self.weighted_sum / np.sum(self.alpha_t[0 : t])
-
+            update = self.weighted_sum / np.sum(self.alpha_t[1 : t])
             return self.f.grad(update)
+            #self.weighted_sum += self.alpha_t[t-1] * x[-1]
+            #update = self.weighted_sum / np.sum(self.alpha_t[0 : t])
+
+            #return self.f.grad(update)
 
             #weighted_sum = np.zeros(shape = (self.d))
             #return self.f.grad(update)
@@ -166,16 +183,16 @@ class OFTL:
 
     def get_update_y(self, x, t):
 
-        if t == 0:
+        if t == 1:
             return self.z0
         else:
             self.weighted_sum += self.alpha_t[t-1] * x[-1]
-            update = (self.weighted_sum + self.alpha_t[t] * x[-1]) / np.sum(self.alpha_t[0:t+1])
+            update = (self.weighted_sum + self.alpha_t[t] * x[-1]) / np.sum(self.alpha_t[1:t+1])
             return self.f.grad(update)
 
 class FTRL:
 
-    def __init__(self, f, d, weights, z0, eta, reg, bounds):
+    def __init__(self, f, d, weights, z0, eta, reg, bounds, prescient = False):
         self.name = "FTRL"
         self.f = f
         self.d = d
@@ -186,10 +203,12 @@ class FTRL:
         self.bounds = bounds
         self.weighted_sum = np.zeros(shape = (self.d))
         #self.weighted_sum += self.z0
+        self.prescient = prescient
 
     def get_update_x(self, y, t):
-        self.weighted_sum += self.alpha_t[t] * y[-1]
-        return self.regularizer.fenchel_grad(-self.eta * self.weighted_sum, 1)
+        if self.prescient:
+            self.weighted_sum += self.alpha_t[t] * y[-1]
+            return self.regularizer.fenchel_grad(-self.eta * self.weighted_sum, 1)
         #return -self.eta * self.weighted_sum 
         #return -self.weighted_sum / np.sqrt(t + 1)
 